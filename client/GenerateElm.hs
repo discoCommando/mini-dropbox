f37a10fb20df8e9f1d8bcdd7d92f31e7a22e26db
discoCommando <tw336034@students.mimuw.edu.pl>
2017-12-06 21:34:46 +0000

    Fixed generate and added to package.yaml

--- client/GenerateElm.hs
+++ client/GenerateElm.hs
@@ -6,21 +6,21 @@ import           Data.Text hiding (intercalate, map)
 import           Elm (toElmDecoderSource, toElmEncoderSource, toElmTypeSource)
 
 import           Models
-import           Api
+import           App
 
 
 main :: IO ()
 main = do
   let code = "module Api exposing (..)" :
             defElmImports :
-            toElmTypeSource (Proxy :: Proxy Item) :
-            toElmDecoderSource (Proxy :: Proxy Item) :
             toElmTypeSource (Proxy :: Proxy File) :
             toElmDecoderSource (Proxy :: Proxy File) :
             toElmTypeSource (Proxy :: Proxy Folder) :
             toElmDecoderSource (Proxy :: Proxy Folder) :
             toElmTypeSource (Proxy :: Proxy FileStructure) :
             toElmDecoderSource (Proxy :: Proxy FileStructure) :
-            generateElmForAPI api
+            toElmTypeSource (Proxy :: Proxy User) :
+            toElmDecoderSource (Proxy :: Proxy User) :
+            generateElmForAPI testApi
   writeFile "client/Api.elm" $ intercalate "\n\n" $ map unpack code  
 
